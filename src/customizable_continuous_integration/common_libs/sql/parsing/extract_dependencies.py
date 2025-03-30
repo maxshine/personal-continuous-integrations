12ad9b63ec2122e9731d39e9a71d42b3d27c22b0
Ryan Gao <ryangao-au@outlook.com>
2025-03-30 16:09:53 +1100

    fix returned dependencies as entities names instead of aliases

--- src/customizable_continuous_integration/common_libs/sql/parsing/extract_dependencies.py
+++ src/customizable_continuous_integration/common_libs/sql/parsing/extract_dependencies.py
@@ -24,7 +24,7 @@ def extract_sql_select_statement_dependencies(sql: str, exclusions: set[str], di
     for node in parsed_sql_ast.find_all(sqlglot.exp.CTE):
         exclusions.add(node.alias_or_name)
     for node in parsed_sql_ast.find_all(sqlglot.exp.Table):
-        full_name = f"{node.catalog+'.' if node.catalog else ''}{node.db+'.' if node.db else ''}{node.alias_or_name}"
+        full_name = f"{node.catalog+'.' if node.catalog else ''}{node.db+'.' if node.db else ''}{node.name}"
         if full_name in exclusions:
             continue
         ret.add(full_name)
