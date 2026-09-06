38aba0d0f9f7587af66c8d364021a03fcf9ed3db
Ryan Gao <ryangao-au@outlook.com>
2026-09-06 10:14:49 +1000

    fix write-protection-pr.sh for github-actions[bot] username

--- scripts/write-protection-pr.sh
+++ scripts/write-protection-pr.sh
@@ -10,6 +10,7 @@
 # -----------------------------------------------------------------------------
 #   01/09/2024     Ryan, Gao       Created this script
 #   03/09/2024     Ryan, Gao       Add more cmdline options preceding env
+#   06/09/2026     Ryan, Gao       Fix github-action[bot] user check
 #
 # Inputs:
 #   Environmental Variables:
@@ -55,7 +56,7 @@ function is_admin_user() {
   OIFS=$IFS
   IFS=";"
   for admin in ${ADMINISTRATIVE_USERS}; do
-    if [[ $1 == $admin ]]; then
+    if [[ "$1" == "$admin" ]]; then
       IFS=$OIFS
       return 1
     fi
